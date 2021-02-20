94506f1c4b7d627185519d8ce42b3afc4edc757a
Habibie Faried <habibiefaried@gmail.com>
2021-02-20 09:09:19 +0700

    add postgresql

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/habibiefaried/database-cloner
 
 go 1.15
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/lib/pq v1.9.0
+	gopkg.in/yaml.v2 v2.4.0
+)
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"errors"
 	"fmt"
+	_ "github.com/lib/pq"
 	"gopkg.in/yaml.v2"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -49,7 +52,7 @@ func main() {
 	// Open config file
 	file, err := os.Open("config.yaml")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 	defer file.Close()
 
@@ -58,31 +61,77 @@ func main() {
 
 	// Start YAML decoding from file
 	if err := d.Decode(&config); err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 
 	if config.Type == "mysql" {
 		command := fmt.Sprintf("MYSQL_PWD=%v mysqldump -h %v -u %v -P %v %v > /tmp/dump.sql", config.Source.Password, config.Source.Host, config.Source.Username, config.Source.Port, config.Source.Database)
 		out, err := runCommandExec(command)
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			log.Fatal(err)
 		}
 		fmt.Println(out)
 
 		command = fmt.Sprintf("MYSQL_PWD=%v mysql -h %v -u %v -P %v -e \"CREATE DATABASE IF NOT EXISTS \\`%v\\` \"", config.Destination.Password, config.Destination.Host, config.Destination.Username, config.Destination.Port, config.Destination.Database)
 		out, err = runCommandExec(command)
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			log.Fatal(err)
 		}
 		fmt.Println(out)
 
 		command = fmt.Sprintf("MYSQL_PWD=%v mysql -h %v -u %v -P %v %v < /tmp/dump.sql", config.Destination.Password, config.Destination.Host, config.Destination.Username, config.Destination.Port, config.Destination.Database)
 		out, err = runCommandExec(command)
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			log.Fatal(err)
+		}
+		fmt.Println(out)
+	} else if config.Type == "psql" {
+		f, err := os.Create("/tmp/.pgpass")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer f.Close()
+
+		dsource := fmt.Sprintf("%v:%v:*:%v:%v\n", config.Source.Host, config.Source.Port, config.Source.Username, config.Source.Password)
+		ddest := fmt.Sprintf("%v:%v:*:%v:%v\n", config.Destination.Host, config.Destination.Port, config.Destination.Username, config.Destination.Password)
+		_, err = f.WriteString(dsource + ddest)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Change permissions Linux.
+		err = os.Chmod("/tmp/.pgpass", 0600)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Writing pgpass done")
+
+		command := fmt.Sprintf("PGPASSFILE='/tmp/.pgpass' pg_dump -h %v -p %v -U %v %v > /tmp/dump.sql", config.Source.Host, config.Source.Port, config.Source.Username, config.Source.Database)
+		out, err := runCommandExec(command)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(out)
+
+		conninfo := fmt.Sprintf("host=%v port=%v user=%v password=%v sslmode=disable", config.Destination.Host, config.Destination.Port, config.Destination.Username, config.Destination.Password)
+		db, err := sql.Open("postgres", conninfo)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = db.Exec(fmt.Sprintf("CREATE DATABASE \"%v\"", config.Destination.Database))
+		if err != nil {
+			log.Println(err)
+		}
+
+		command = fmt.Sprintf("PGPASSFILE='/tmp/.pgpass' psql -h %v -p %v -d %v -U %v -f /tmp/dump.sql", config.Destination.Host, config.Destination.Port, config.Destination.Database, config.Destination.Username)
+		out, err = runCommandExec(command)
+		if err != nil {
+			log.Fatal(err)
 		}
 		fmt.Println(out)
 	}
